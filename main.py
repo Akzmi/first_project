1a63ee54fa434b21bd97192668cd9f6d6e172557
Amir <amir@gmail.com>
2024-05-05 19:15:17 +0330

    merging?

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ print("Hello world!!!!!!!!!!!!!!")
 print("how are you?")
 
 print("Hello world!!!!!!!!!!!!!!")
-print("Hello world!!!!!!!!!!!!!!")
+print("Hello branch1!!!!!!!!!!!!!!")
+print("Hello branch2222222!!!!!!!!")
