22eda8efc2a5e787d63497b95b50c2416e6e532a
Amir <amir@gmail.com>
2024-05-05 13:32:33 +0330

    adding bug-fixes branch

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ print("Hello world!!!!!!!!!!!!!!")
 print("how are you?")
 print("this is a new branch")
 print("this is a new branch2")
-print("this is a new branch2")
+
 
 print("Hello world!!!!!!!!!!!!!!")print("Hello world!!!!!!!!!!!!!!")
