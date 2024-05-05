f6a5444c32ccc67f248a977b896ec3966dc3cd75
Amir <amir@gmail.com>
2024-05-05 15:58:38 +0330

    explain main2

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 print("Hello world!!!!!!!!!!!!!!")
 print("how are you?")
-print("this is a new branch")
-print("this is a new branch2")
-print("this is a new branch2")
 
 print("Hello world!!!!!!!!!!!!!!")
 print("Hello main!!!!!!!!!!!!!!")
