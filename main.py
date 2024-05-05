01274f336d06ba0a731677ed1d7e18dd183ff2da
Amir <amir@gmail.com>
2024-05-05 15:55:20 +0330

    explain tt-test

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ print("this is a new branch")
 print("this is a new branch2")
 print("this is a new branch2")
 
-print("Hello world!!!!!!!!!!!!!!")print("Hello world!!!!!!!!!!!!!!")
+print("Hello world!!!!!!!!!!!!!!")
+
+
+print("Hello tt_test!!!!!!!!!!!!!!")
