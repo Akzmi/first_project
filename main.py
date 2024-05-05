a60fe9373ee9a021611ea6a03939341977f0d6f1
Amir <amir@gmail.com>
2024-05-05 12:54:15 +0330

    addtest that brnch tt-test is not effecting main

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ print("how are you?")
 print("this is a new branch")
 print("this is a new branch2")
 print("this is a new branch2")
+
+print("Hello world!!!!!!!!!!!!!!")print("Hello world!!!!!!!!!!!!!!")
